28f034686cd4187006cd0e54400f55f1cd17a0ab
focus617 <13980668829@163.com>
2018-12-29 14:53:05 +0800

    configure Postgres, get it up and running in another container, and link it to the users service

--- services/users/manage.py
+++ services/users/manage.py
@@ -12,5 +12,16 @@ to the Flask app, in order to run and manage the app from the command line
 cli = FlaskGroup(app)
 
 
+"""
+This registers a new command, recreate_db, to the CLI so that we can run it from 
+the command line, which we'll use to apply the model to the database.
+"""
+@cli.command()
+def recreate_db():
+    db.drop_all()
+    db.create_all()
+    db.session.commit()
+
+
 if __name__ == '__main__':
     cli()
